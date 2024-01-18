b2dcfc98573a0ad3b75cf1460dddd2c21b489a01
akshat <akshatbhatya@gmail.com>
2024-01-18 20:47:52 +0530

    add card componet

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Product.css"
+
+function Product() {
+  return (
+    <div className='card'>
+        <div className="card_image">
+            <img src="" alt="image" />
+        </div>
+        <div className="card_content">
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+        </div>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/Components/ProductBody/ProductBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProductBody.css"
+
+function ProductBody() {
+  return (
+    <div className='product_list'>
+      
+    </div>
+  )
+}
+
+export default ProductBody
